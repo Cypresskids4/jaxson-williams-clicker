eb2ccb93ee796d3ef69711b11904999561187f4a
Cypresskids4 <134014357+Cypresskids4@users.noreply.github.com>
2023-05-22 18:02:01 -0500

    Add files via upload

---
+++ lists.js
@@ -0,0 +1,17 @@
+export let dimensions = [
+    "multiverse images/Space.jpg",
+    "./multiverse-images/Pancake.jpg",
+    "./multiverse-images/Fortnite.jpg",
+    "./multiverse-images/Minecraft.jpg",
+    "./multiverse-images/Cobra.jpg",
+    "./multiverse-images/Demon.jpg",
+    "./multiverse-images/Earth.jpg",
+];
+export let dimensionPriceList = [
+    10000, //pancake place ten thousand
+    100000, //fortnite one hundred thousand
+    1000000, //minecraft one million
+    10000000, //sanke ten million
+    100000000, //demon-realm one hundred million
+    1000000000 //earth one billion
+];
